88e2147e3e7e29a33bf6848da0b9d37317250902
joseph.damarseto@gmail.com <joseph.damarseto@gmail.com>
2024-09-30 03:52:48 +0700

    mark

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
-import { SimpleMarkerSymbol } from '@arcgis/core/symbols';
 import Map from '@arcgis/core/Map';
-import Graphic from '@arcgis/core/Graphic';
 import MapView from '@arcgis/core/views/MapView';
+import Graphic from '@arcgis/core/Graphic';
 import Point from '@arcgis/core/geometry/Point';
+import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
 
 @Component({
@@ -15,49 +15,47 @@ import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
 export class HomePage implements OnInit {
   mapView: MapView | any;
   userLocationGraphic: Graphic | any;
-  map: Map | any;
+  initialUserLocationSet: boolean = false; // Flag untuk cek apakah lokasi pengguna pertama sudah di-set
+  selectedBasemap: string = 'topo-vector'; // Default basemap
+  basemaps: any[] = [
+    { value: 'topo-vector', label: 'Topographic' },
+    { value: 'streets', label: 'Streets' },
+    { value: 'satellite', label: 'Satellite' },
+    { value: 'hybrid', label: 'Hybrid' },
+    { value: 'terrain', label: 'Terrain' },
+    { value: 'dark-gray', label: 'Dark Gray' },
+    { value: 'oceans', label: 'Oceans' }
+  ];
 
-  constructor() {}
+  constructor() { }
 
   async ngOnInit() {
-    //  instance peta dengan basemap
-    this.map = new Map({
-      basemap: "topo-vector",
+    const map = new Map({
+      basemap: this.selectedBasemap, // Basemap awal
     });
 
     this.mapView = new MapView({
-      container: "container",
-      map: this.map,
-      zoom: 8,
+      container: 'container',
+      map: map,
+      zoom: 10, // Zoom untuk fokus di area yang diinginkan
+      center: [-7.758391153446039, 110.37287184338194],
     });
 
+    // Layer informasi cuaca
+    let weatherServiceFL = new ImageryLayer({ url: WeatherServiceUrl });
+    map.add(weatherServiceFL);
 
-    let weatherServiceFL = new ImageryLayer({ url: WeatherServiceURL });
-    this.map.add(weatherServiceFL);
-
-
+    // Update lokasi pengguna secara periodik
     await this.updateUserLocationOnMap();
-    this.mapView.center = this.userLocationGraphic.geometry as Point;
-
-
     setInterval(this.updateUserLocationOnMap.bind(this), 10000);
 
-
-    this.mapView.on("click", (event: any) => {
-      if (event.button === 2) {
-        this.showCoordinatesOnRightClick(event.mapPoint);
-      }
-    });
+    // Tambahkan marker cuaca
+    this.addWeatherMarker();
   }
 
-
-  onBasemapChange(event: any) {
-    const selectedBasemap = event.target.value;
-    this.map.basemap = selectedBasemap;
-  }
-
+  // Menggunakan plugin Capacitor Geolocation untuk mendapatkan lokasi pengguna
   async getLocationService(): Promise<number[]> {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       navigator.geolocation.getCurrentPosition((resp) => {
         resolve([resp.coords.latitude, resp.coords.longitude]);
       });
@@ -65,28 +63,61 @@ export class HomePage implements OnInit {
   }
 
   async updateUserLocationOnMap() {
-    let geom = new Point({
-      longitude: -78.24908214885481,
-      latitude: 40.39682132573387,
-    });
+    let latLng = await this.getLocationService();
+    let geom = new Point({ latitude: latLng[0], longitude: latLng[1] });
 
     if (this.userLocationGraphic) {
+      // Update lokasi marker pengguna tanpa mengubah pusat peta
       this.userLocationGraphic.geometry = geom;
     } else {
+      // Buat marker pengguna jika belum ada
       this.userLocationGraphic = new Graphic({
-        symbol: new SimpleMarkerSymbol(),
+        symbol: new SimpleMarkerSymbol({
+          color: 'red',
+          size: '10px',
+          outline: {
+            color: 'white',
+            width: 2,
+          },
+        }),
         geometry: geom,
       });
       this.mapView.graphics.add(this.userLocationGraphic);
+
+      // Hanya memusatkan peta ke lokasi pengguna saat pertama kali di-load
+      if (!this.initialUserLocationSet) {
+        this.mapView.center = geom;
+        this.mapView.zoom = 14; // Bisa disesuaikan jika diperlukan
+        this.initialUserLocationSet = true; // Flag untuk mencegah reset saat pembaruan lokasi berikutnya
+      }
     }
   }
 
-  
-  showCoordinatesOnRightClick(mapPoint: Point) {
-    const latitude = mapPoint.latitude;
-    const longitude = mapPoint.longitude;
-    alert(`Koordinat: Latitude: ${latitude}, Longitude: ${longitude}`);
+  changeBasemap() {
+    this.mapView.map.basemap = this.selectedBasemap;
+  }
+
+  // Fungsi untuk menambahkan marker di area cuaca tertentu
+  addWeatherMarker() {
+    let weatherPoint = new Point({
+      latitude: 40.98459970225305, // Lokasi yang tertutup cuaca
+      longitude:  -81.54566570100829,
+    });
+
+    let weatherMarker = new Graphic({
+      geometry: weatherPoint,
+      symbol: new SimpleMarkerSymbol({
+        color: 'blue',
+        size: '25px',
+        outline: {
+          color: 'white',
+          width: 2,
+        },
+      }),
+    });
+
+    this.mapView.graphics.add(weatherMarker);
   }
 }
 
-const WeatherServiceURL = 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer';
+const WeatherServiceUrl = 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer';
